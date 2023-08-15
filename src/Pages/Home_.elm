b3a956899008b0a4c828dbba397c0aaca9489efc
Jason Wang <jasonprime80704@gmail.com>
2023-08-15 12:14:26 -0400

    feat: add sign in page, no routing yet

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,11 +1,36 @@
 module Pages.Home_ exposing (page)
 
 import Html
+import Html.Attributes as Attr
 import View exposing (View)
 
 
 page : View msg
 page =
-    { title = "Homepage"
-    , body = [ Html.text "Hello, world!" ]
+    { title = "Board Game Tracker"
+    , body =
+        [ Html.div
+            [ Attr.class "m-6" ]
+            [ Html.section
+                [ Attr.class "" ]
+                [ Html.div
+                    [ Attr.class "" ]
+                    [ Html.p
+                        [ Attr.class "title" ]
+                        [ Html.text "Board Game Tracker" ]
+                    , Html.p
+                        [ Attr.class "subtitle" ]
+                        [ Html.text "Track your board games with friends!" ]
+                    ]
+                ]
+            , Html.div
+                [ Attr.class "mt-3" ]
+                [ Html.a
+                    [ Attr.class "button is-info"
+                    , Attr.href "/sign-in"
+                    ]
+                    [ Html.text "Login" ]
+                ]
+            ]
+        ]
     }
